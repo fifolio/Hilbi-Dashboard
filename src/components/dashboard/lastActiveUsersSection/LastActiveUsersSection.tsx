bec211cdddeedd4d0b19caffc4507a731fa47e1f
fifolio <firasdabbabi@gmail.com>
2025-11-23 20:53:43 +0100

    Feat: Integrate Zustand Store and API Logic for Last Active Users
    This commit completes the data integration layer for the 'Last Active Users' functionality, ensuring data is managed efficiently and fetched correctly.
    
    -> State Management Integration: A Zustand store has been added and configured to handle the generalization of user data, allowing various components across the project to easily access and subscribe to the latest user information.
    
    -> API Data Fetching: The main API call function to retrieve the last active users from the designated endpoint has been fully implemented.
    
    -> Error Handling: All expected returns and potential errors from the API endpoint have been properly handled within the fetch function, improving application stability.
    
    -> Helper Utilities: A new /helper folder was created, containing the formatDate function, which transforms the users' last update date into the required display format.

--- src/components/dashboard/lastActiveUsersSection/LastActiveUsersSection.tsx
+++ src/components/dashboard/lastActiveUsersSection/LastActiveUsersSection.tsx
@@ -1,21 +1,55 @@
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "@tanstack/react-router";
 import UserDetails from "./UserDetails";
+import { useEffect, useState } from "react";
+import { fetchLastActiveUsers } from "@/services";
+import { Spin } from "antd";
 
 export default function LastActiveUsersSection() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [lastActiveUsers, setLastActiveUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      // delay 2 seconds to simulate API latency
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const users = await fetchLastActiveUsers();
+      setLastActiveUsers(users || []);
+      setLoading(false);
+    };
+
+    loadUsers();
+  }, []);
 
   return (
     <div className="tw:w-full tw:h-[495px] tw:rounded-xl tw:py-3 tw:bg-white tw:border tw:border-gray-200 tw:text-black">
       <div className="tw:px-3 tw:flex tw:justify-between tw:items-center tw:pb-3">
         <h6 className="tw:font-semibold">Last active users</h6>
-        <Link to="/users/list" className="tw:text-gray-500 tw:text-[12px] tw:flex tw:space-x-1 tw:items-center">
+        <Link
+          to="/users/list"
+          className="tw:text-gray-500 tw:text-[12px] tw:flex tw:space-x-1 tw:items-center"
+        >
           <span>All users</span> <RightOutlined />
         </Link>
       </div>
+
       <hr className="tw:text-gray-200" />
+
       <div className="tw:h-[448px] tw:overflow-y-auto">
-        <UserDetails />
+        {loading ? (
+          <center className="tw:h-full tw:flex tw:items-center tw:justify-center">
+            <Spin size="large" />
+          </center>
+        ) : lastActiveUsers && lastActiveUsers.length > 0 ? (
+          lastActiveUsers.map((user) => (
+            <UserDetails key={user.id} data={user} />
+          ))
+        ) : (
+          <center className="tw:h-full tw:flex tw:items-center tw:justify-center">
+            <span className="tw:text-gray-500 tw:italic">No users found.</span>
+          </center>
+        )}
       </div>
     </div>
-  )
+  );
 }
