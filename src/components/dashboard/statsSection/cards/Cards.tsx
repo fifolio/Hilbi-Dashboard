482410ab61c953814addd63a01af6236e89618fb
fifolio <firasdabbabi@gmail.com>
2025-11-24 03:01:08 +0100

    Feat: Integrate Dashboard State Cards with API Endpoints
    This commit completes the integration of the Dashboard State Cards with their respective API endpoints, allowing them to display live data.
    
    API Functions Implemented: Dedicated fetching functions were created:
    -> fetchTotalUsers
    -> fetchTotalInactiveUsers
    -> fetchTotalActiveUsers
    
    Data Handling: The Card component now properly handles the asynchronous fetching process, ensuring the accurate and timely display of user state metrics (Total, Active, and Inactive Users) from the backend.

--- src/components/dashboard/statsSection/cards/Cards.tsx
+++ src/components/dashboard/statsSection/cards/Cards.tsx
@@ -1,10 +1,36 @@
 import type { CardStructure } from '@/interfaces'
+import { fetchTotalActiveUsers, fetchTotalInactiveUsers, fetchTotalUsers } from '@/services';
+import { Spin } from 'antd';
+import { useEffect, useState } from 'react';
 
 type CardsProps = {
   cardStructure: CardStructure
 }
 
 export default function Cards({ cardStructure }: CardsProps) {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [totalUsers, setTotalUsers] = useState<number | null>(null);
+  const [activeUsers, setActiveUsers] = useState<number | null>(null);
+  const [InactiveUsers, setInactiveUsers] = useState<number | null>(null);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+
+      const totalUsers = await fetchTotalUsers();
+      const activeUsers = await fetchTotalActiveUsers();
+      const InactiveUsers = await fetchTotalInactiveUsers();      
+      
+      setTotalUsers(totalUsers);
+      setActiveUsers(activeUsers);
+      setInactiveUsers(InactiveUsers);
+
+      setLoading(false);
+    };
+
+    loadUsers();
+  }, []);
+
   return (
     <div className="tw:w-full tw:h-[118px] tw:rounded-xl tw:py-3 tw:bg-white tw:border tw:border-gray-200 tw:space-y-2 tw:text-black">
       <div className="tw:px-3 tw:flex tw:justify-between tw:items-center">
@@ -13,7 +39,14 @@ export default function Cards({ cardStructure }: CardsProps) {
       </div>
       <hr className="tw:text-gray-200" />
       <div className="tw:px-3 tw:mt-3">
-        <h2 className="tw:text-[24px] tw:!font-semibold">123</h2>
+        <h2 className="tw:text-[24px] tw:!font-semibold">
+          {loading ?
+            <Spin size="small" /> :
+            cardStructure.title === 'Total users' ? totalUsers :
+              cardStructure.title === 'Active users' ? activeUsers :
+                cardStructure.title === 'Inactive users' ? InactiveUsers :
+                  null}
+        </h2>
         <h6 className="tw:text-gray-500 tw:text-[12px]">
           {cardStructure.description}
         </h6>
